91411543533a9e2c6c0a3a727c9c0210cd3aab25
jorsi <jonathon.orsi@gmail.com>
2020-02-04 14:29:13 -0500

    remove unused reference

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const webpack = require('webpack');
 
 module.exports = {
   entry: './src/betical.js',
